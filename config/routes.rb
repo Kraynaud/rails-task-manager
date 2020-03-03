af4c43fc46146e0c270adc7e987452f60968094d
kevin <kraynaud@gmail.com>
2020-03-03 15:50:19 +0100

    done and functionnal

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create', as: :create
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+
+  delete 'tasks/:id', to: 'tasks#delete', as: :delete
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
